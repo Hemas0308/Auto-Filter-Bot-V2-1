0c3ee4e50a482d34bda9bfadd022f9facd6f3260
Hemas0308 <81526699+Hemas0308@users.noreply.github.com>
2021-06-08 19:28:05 +0400

    Update script.py

--- script.py
+++ script.py
@@ -14,17 +14,34 @@ class script(object):
 For more click <i>help</i></b>"""
 
 
-    Group_MSG = """<b>https://t.me/aedahamlibrary_noolakam</b>"""
+    HELP_MSG = """<b>How to use the bot??</b>
+<i>
+* Add bot to your group with admin rights.
+* Add bot to channels which you want to link with <b>all admin rights</b>!
+</i>
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+* <code>/add channelid</code>  -  Links channel to your group.
+or
+* <code>/add @channelusername</code> - Links channel to your group.
+<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+* <code>/del channelid</code>  -  Delinks channel from group
+or
+* <code>/del @channelusername</code>  -  Delinks channel from group
+<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+* <code>/delall</code>  -  Removes all connected channels and filters from group!
+* <code>/filterstats</code>  -  Check connected channels and number of filters.
+No need add each filter again!
+Bot will automatically search for your files and give links to that!
+<b>© @librarytamil</b>"""
 
 
-    Channel_MSG = """⭕️<b>https://t.me/aedahamlibrary1</b>
+    ABOUT_MSG = """⭕️<b>https://telegram.me/aedahamlibrary_noolakam</b>
     
 ⭕️<b>Creater :</b> @kaimudinjavai
-
 ⭕️<b>Language :</b> <code>Python3</code>
-
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
 ⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+
 
 """
