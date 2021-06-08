b8f8429dd83d2d40c7a4a697cfc538b9169596eb
Hemas0308 <81526699+Hemas0308@users.noreply.github.com>
2021-06-08 19:17:56 +0400

    Update script.py

--- script.py
+++ script.py
@@ -14,49 +14,10 @@ class script(object):
 For more click <i>help</i></b>"""
 
 
-    குழுவில் இணைய_MSG = """<b>How to use the bot??</b>
-
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
+    குழுவில் இணைய_MSG = """<b>https://t.me/aedahamlibrary_noolakam</b>"""
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @librarytamil</b>"""
-
-
-    சேனலில் இணைய = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    சேனலில் இணைய = """⭕️<b>https://t.me/aedahamlibrary1</b>
     
 ⭕️<b>Creater :</b> @kaimudinjavai
 
